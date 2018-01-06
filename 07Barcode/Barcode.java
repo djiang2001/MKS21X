3def011781d15dbb1e2c865347b6c953d26eb99e
djiang2 <djiang2>
2018-01-05 19:44:20 -0500

    Deleted excess code that originated from unknown place and tested

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,78 +1,4 @@
 public class Barcode implements Comparable<Barcode>{
-<<<<<<< HEAD
-    private String zip;
-    //--Constructor--//
-
-    public Barcode(String ZIP){
-	if(ZIP.length() != 5){
-	    throw new IllegalArgumentException();
-	}
-	for(int i =0;x <ZIP.length();i++){
-	    if(!Character.isDigit(ZIP.charAt(i)){
-		    throw new IllegalArgumentException();
-		}
-	zip = ZIP;
-    }
-
-    //--Accessors--//
-
-    public String getZip(){
-	return zip;
-    }
-
-    public String getCode(){
-	return toCode(zip);
-    }
-
-    public String getBarcode(){
-	return this.toBar();
-    }
-
-    //--Methods--//
-    public static String toCode(String zip){
-	
-    }
-
-    public static String toZip(String code){
-	
-    }
-    
-    public String toString(){
-	return getBarcode() + " " + "("+ getZip() + ")";
-    }
-
-    public int compareTo(Barcode other){
-	return getZip().compareTo(other.getZip());
-    }
-
-    public boolean equals(Barcode other){
-	return getZip().equals(other.getZip());
-    }
-
-    //--Helpers--//
-
-    private String toBar(){
-	String result = "|";
-	for(int i = 0; i < zip.length();i++){
-	    result += toBarcode(getZip().charAt(i));
-	}
-	result += toBarcode(checkSum()) + "|";
-	return result;
-    }
-
-    private char checkSum(){
-	int result = 0;
-	for(int i = 0;i < zip.length(); i++){
-	    result += Character.getNumericValue(getZip().charAt(i));
-	}
-	return (char)('0' + (result % 10));
-    }
-
-    private String toBarcode(char a){
-	String[] key = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
-	return key[Character.getNumericValue(a)];
-    }
-=======
   private String zip;
   //--Constructor--//
 
@@ -174,5 +100,4 @@ public class Barcode implements Comparable<Barcode>{
     return sum % 10;
   }
 
->>>>>>> 90d66d72d4e60c362d373dab3c999dcff255f1d9
 }
--- 07Barcode/Driver.java
+++ 07Barcode/Driver.java
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-public class Driver{
-  public static void main(String[]args){
-    Barcode home = new Barcode("10324");
-    Barcode school = new Barcode("13456");
-    System.out.println(home);//|:::||||:::::||:::|:|:|::|||:::| (10324)
-    System.out.println(home.getZip());//1032
-    System.out.println(home.getBarcode());//|:::||||:::::||:::|:|:|::|||:::|
-    System.out.println(school.getBarcode());//|:::||::||::|::|:|:|::||::|:|::|
-    System.out.println(home.equals(school));//false
-    System.out.println(home.compareTo(school));//-3
-  }
-=======
+
+
 public class Driver {
     public static void main(String[] args) {
         Barcode A = new Barcode("08451");
@@ -38,5 +27,5 @@ public class Driver {
         System.out.println("|:|:|::::||:|::|||::::::||:::||| : " + Barcode.toZip("|:|:|::::||:|::|||::::::||:::|||")); // 51401
         System.out.println("51401 : " + Barcode.toCode("51401")); // |:|:|::::||:|::|||::::::||:::|||
     }
->>>>>>> 90d66d72d4e60c362d373dab3c999dcff255f1d9
+
 }
